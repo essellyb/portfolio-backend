9cf28b10ba040af668f94a9f1b5280e0ea2eea5b
david <mresselll2003@gmail.com>
2026-08-12 16:26:35 +0100

    add contact response dto

---
+++ src/main/java/com/essel/portfolio/dto/ContactResponse.java
@@ -0,0 +1,14 @@
+package com.essel.portfolio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactResponse {
+
+    private boolean success;
+    private String message;
+}
